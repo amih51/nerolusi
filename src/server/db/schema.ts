01a50c6dc58bbd1aea78b8244e2d56124add19f7
hima <19623185@std.stei.itb.ac.id>
2024-10-13 02:12:57 +0700

    feat: mysql -> postgresql

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,49 +1,42 @@
 import { relations } from "drizzle-orm";
 import {
-  int,
   timestamp,
-  mysqlTable,
+  pgTable,
+  text,
   primaryKey,
-  varchar,
-  mysqlEnum,
+  integer,
+  serial,
+  pgEnum,
   time,
-} from "drizzle-orm/mysql-core";
+} from "drizzle-orm/pg-core";
 import type { AdapterAccountType } from "next-auth/adapters";
 
-export const users = mysqlTable("user", {
-  id: varchar("id", { length: 255 })
+export const users = pgTable("user", {
+  id: text("id")
     .primaryKey()
     .$defaultFn(() => crypto.randomUUID()),
-  name: varchar("name", { length: 255 }),
-  email: varchar("email", { length: 255 }).unique(),
-  emailVerified: timestamp("emailVerified", {
-    mode: "date",
-    fsp: 3,
-  }),
-  image: varchar("image", { length: 255 }),
-  role: mysqlEnum(["student", "teacher", "admin"])
-    .notNull()
-    .$default(() => "student"),
+  name: text("name"),
+  email: text("email").unique(),
+  emailVerified: timestamp("emailVerified", { mode: "date" }),
+  image: text("image"),
 });
 
-export const accounts = mysqlTable(
+export const accounts = pgTable(
   "account",
   {
-    userId: varchar("userId", { length: 255 })
+    userId: text("userId")
       .notNull()
       .references(() => users.id, { onDelete: "cascade" }),
-    type: varchar("type", { length: 255 })
-      .$type<AdapterAccountType>()
-      .notNull(),
-    provider: varchar("provider", { length: 255 }).notNull(),
-    providerAccountId: varchar("providerAccountId", { length: 255 }).notNull(),
-    refresh_token: varchar("refresh_token", { length: 255 }),
-    access_token: varchar("access_token", { length: 255 }),
-    expires_at: int("expires_at"),
-    token_type: varchar("token_type", { length: 255 }),
-    scope: varchar("scope", { length: 255 }),
-    id_token: varchar("id_token", { length: 2048 }),
-    session_state: varchar("session_state", { length: 255 }),
+    type: text("type").$type<AdapterAccountType>().notNull(),
+    provider: text("provider").notNull(),
+    providerAccountId: text("providerAccountId").notNull(),
+    refresh_token: text("refresh_token"),
+    access_token: text("access_token"),
+    expires_at: integer("expires_at"),
+    token_type: text("token_type"),
+    scope: text("scope"),
+    id_token: text("id_token"),
+    session_state: text("session_state"),
   },
   (account) => ({
     compoundKey: primaryKey({
@@ -52,43 +45,70 @@ export const accounts = mysqlTable(
   }),
 );
 
-export const sessions = mysqlTable("session", {
-  sessionToken: varchar("sessionToken", { length: 255 }).primaryKey(),
-  userId: varchar("userId", { length: 255 })
+export const sessions = pgTable("session", {
+  sessionToken: text("sessionToken").primaryKey(),
+  userId: text("userId")
     .notNull()
     .references(() => users.id, { onDelete: "cascade" }),
   expires: timestamp("expires", { mode: "date" }).notNull(),
 });
 
-export const classes = mysqlTable("class", {
-  id: int("id").primaryKey().autoincrement(),
-  name: varchar("name", { length: 255 }).notNull(),
+export const verificationTokens = pgTable(
+  "verificationToken",
+  {
+    identifier: text("identifier").notNull(),
+    token: text("token").notNull(),
+    expires: timestamp("expires", { mode: "date" }).notNull(),
+  },
+  (verificationToken) => ({
+    compositePk: primaryKey({
+      columns: [verificationToken.identifier, verificationToken.token],
+    }),
+  }),
+);
+
+export const classes = pgTable("class", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
 });
 
-export const packages = mysqlTable("package", {
-  id: int("id").primaryKey().autoincrement(),
-  name: varchar("name", { length: 255 }).notNull(),
-  type: mysqlEnum(["tryout", "drill"]).notNull(),
+export const typeEnum = pgEnum("type", ["tryout", "drill"]);
+
+export const packages = pgTable("package", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+  type: typeEnum().notNull(),
   TOstart: timestamp("start"),
   TOend: timestamp("end"),
   TOduration: time("duration"),
-  classId: int("classId").references(() => classes.id, {
+  classId: integer("classId").references(() => classes.id, {
     onDelete: "cascade",
   }),
 });
 
-export const questions = mysqlTable("question", {
-  id: varchar("id", { length: 255 })
+export const subtestEnum = pgEnum("subtest", [
+  "pu",
+  "ppu",
+  "pbm",
+  "pk",
+  "lb",
+  "pm",
+]);
+
+export const questionTypeEnum = pgEnum("question_type", ["essay", "mulChoice"]);
+
+export const questions = pgTable("question", {
+  id: text("id")
     .primaryKey()
     .$defaultFn(() => crypto.randomUUID()),
-  content: varchar("content", { length: 2048 }).notNull(),
-  subtest: mysqlEnum(["pu", "ppu", "pbm", "pk", "lb", "pm"]).notNull(),
-  type: mysqlEnum(["essay", "mulChoice"]).notNull(),
-  score: int("score").default(0),
-  correctAnswerId: varchar("correct_answer", { length: 255 }),
+  content: text("content").notNull(),
+  subtest: subtestEnum().notNull(),
+  type: questionTypeEnum().notNull(),
+  score: integer("score").default(0),
+  correctAnswerId: text("correct_answer"),
   createdAt: timestamp("created_at").defaultNow().notNull(),
-  packageId: int("packageId")
+  packageId: integer("packageId")
     .notNull()
     .references(() => packages.id, { onDelete: "cascade" }),
 });
@@ -101,32 +121,29 @@ export const questionsRelation = relations(questions, ({ one }) => ({
   }),
 }));
 
-export const answers = mysqlTable("answer", {
-  id: varchar("id", { length: 255 })
+export const answers = pgTable("answer", {
+  id: text("id")
     .primaryKey()
     .$defaultFn(() => crypto.randomUUID()),
-  content: varchar("content", { length: 2048 }).notNull(),
-  questionId: varchar("questionId", { length: 255 })
+  content: text("content").notNull(),
+  questionId: text("questionId")
     .notNull()
     .references(() => questions.id, { onDelete: "cascade" }),
 });
 
-export const userAnswers = mysqlTable(
+export const userAnswers = pgTable(
   "user_answer",
   {
-    questionId: varchar("questionId", { length: 255 })
+    questionId: text("questionId")
       .notNull()
       .references(() => questions.id, { onDelete: "cascade" }),
-    userId: varchar("userId", { length: 255 })
+    userId: text("userId")
       .notNull()
       .references(() => users.id, { onDelete: "cascade" }),
-    answerId: varchar("answerId", { length: 255 }).references(
-      () => answers.id,
-      {
-        onDelete: "cascade",
-      },
-    ),
-    answer: varchar("content", { length: 2048 }).notNull(),
+    answerId: text("answerId").references(() => answers.id, {
+      onDelete: "cascade",
+    }),
+    answer: text("content").notNull(),
   },
   (t) => ({
     pk: primaryKey({ columns: [t.userId, t.questionId] }),
