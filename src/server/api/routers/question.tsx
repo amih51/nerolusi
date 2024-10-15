4f0babce5003f82463d39256a42a2e292b924bf9
hima <19623185@std.stei.itb.ac.id>
2024-10-15 16:42:48 +0700

    feat: add trpc panel

--- src/server/api/routers/question.tsx
+++ src/server/api/routers/question.tsx
@@ -2,20 +2,18 @@
 
 import {
   createTRPCRouter,
-  // protectedProcedure,
+  protectedProcedure,
   publicProcedure,
 } from "~/server/api/trpc";
-import { db } from "~/server/db";
 
 export const questionsRouter = createTRPCRouter({
-  getAllQuestions: publicProcedure.query(async () => {
-    const questions = await db.query.questions.findMany();
+  getAllQuestions: protectedProcedure.query(async ({ ctx }) => {
+    const questions = await ctx.db.query.questions.findMany();
     return questions ?? null;
   }),
 
   getOneQuestion: publicProcedure.query(async ({ ctx }) => {
     const question = await ctx.db.query.questions.findFirst();
-
     return question ?? null;
   }),
 });
