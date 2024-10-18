ec71eef8c546cde1911f92ce6c5e20b5d4cb73bc
hima <19623185@std.stei.itb.ac.id>
2024-10-18 20:21:32 +0700

    feat: add file upload api

--- src/server/api/routers/file.tsx
+++ src/server/api/routers/file.tsx
@@ -1,8 +1,29 @@
+import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+import { db } from "~/server/db";
+import { files } from "~/server/db/schema";
 
 export const fileRouter = createTRPCRouter({
   getAllFiles: protectedProcedure.query(async ({ ctx }) => {
     const files = await ctx.db.query.files.findMany();
     return files ?? null;
   }),
+
+  uploadFile: protectedProcedure
+    .input(
+      z.object({
+        title: z.string(),
+        description: z.string().optional(),
+        url: z.string().url(),
+      }),
+    )
+    .mutation(async ({ input }) => {
+      const result = await db.insert(files).values({
+        title: input.title,
+        description: input.description,
+        url: input.url,
+      });
+
+      return result;
+    }),
 });
