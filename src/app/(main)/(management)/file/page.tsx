be9619c47c7bcb7cf1b636c073f3ee8cf1527dbf
hima <19623185@std.stei.itb.ac.id>
2024-10-18 12:33:36 +0700

    feat: add file uploader

--- src/app/(main)/(management)/file/page.tsx
+++ src/app/(main)/(management)/file/page.tsx
@@ -1,3 +1,20 @@
+"use client";
+import { UploadButton } from "~/utils/uploadthing";
+
 export default function Page() {
-  return <div>Manaj User</div>;
+  return (
+    <div className="flex size-full items-center justify-center">
+      <UploadButton
+        className="border"
+        endpoint="fileUploader"
+        onClientUploadComplete={(res) => {
+          console.log("Files: ", res);
+          alert("Upload Completed");
+        }}
+        onUploadError={(error: Error) => {
+          alert(`ERROR! ${error.message}`);
+        }}
+      />
+    </div>
+  );
 }
--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -3,25 +3,23 @@
 /* eslint-disable @typescript-eslint/await-thenable */
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
+import { auth } from "~/server/auth";
 
 const f = createUploadthing();
 
-const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
-
-// FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
   imageUploader: f({ image: { maxFileSize: "4MB" } })
     // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
       // This code runs on your server before upload
-      const user = await auth(req);
+      const session = await auth();
 
       // If you throw, the user will not be able to upload
-      if (!user) throw new UploadThingError("Unauthorized");
+      if (!session) throw new UploadThingError("Unauthorized");
 
       // Whatever is returned here is accessible in onUploadComplete as `metadata`
-      return { userId: user.id };
+      return { userId: session?.user.id };
     })
     .onUploadComplete(async ({ metadata, file }) => {
       // This code RUNS ON YOUR SERVER after upload
@@ -30,6 +28,21 @@ export const ourFileRouter = {
       console.log("file url", file.url);
 
       // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+      return { uploadedBy: metadata.userId };
+    }),
+  fileUploader: f({ pdf: { maxFileSize: "4MB" } })
+    .middleware(async ({ req }) => {
+      const session = await auth();
+
+      if (!session) throw new UploadThingError("Unauthorized");
+
+      return { userId: session?.user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      console.log("Upload complete for userId:", metadata.userId);
+
+      console.log("file url", file.url);
+
       return { uploadedBy: metadata.userId };
     }),
 } satisfies FileRouter;
